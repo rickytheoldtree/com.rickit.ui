1b20bd08affe1c8e566f7c1965501e2c14492b74
Li yiqi <liyq@zhongbosoft.com>
2024-02-06 10:50:12 +0800

    v1.1.0

--- Editor/UIManagerEditorHelper.cs
+++ Editor/UIManagerEditorHelper.cs
@@ -1,25 +1,19 @@
-﻿using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RicKit.UI.Editor
 {
     public static class UIManagerEditorHelper
     {
-        //检测是否存在Config,如果不存在则创建
-        [UnityEditor.InitializeOnLoadMethod]
-        private static void CheckConfig()
+        [UnityEditor.MenuItem("RicKit/UI/Create UISettings")]
+        private static void CreateSettings()
         {
-            //是否任意Resources文件夹下存在UIManagerConfig
-            if (!UnityEditor.AssetDatabase.FindAssets("t:UISettings", new []{"Assets"}).Any())
+            var config = ScriptableObject.CreateInstance<UISettings>();
+            if (!UnityEditor.AssetDatabase.IsValidFolder("Assets/Resources"))
             {
-                
-                var config = ScriptableObject.CreateInstance<UISettings>();
-                if (!UnityEditor.AssetDatabase.IsValidFolder("Assets/Resources"))
-                {
-                    UnityEditor.AssetDatabase.CreateFolder("Assets", "Resources");
-                }
-                UnityEditor.AssetDatabase.CreateAsset(config, "Assets/Resources/UISettings.asset");
+                UnityEditor.AssetDatabase.CreateFolder("Assets", "Resources");
             }
+            UnityEditor.AssetDatabase.CreateAsset(config, "Assets/Resources/UISettings.asset");
+            UnityEditor.Selection.activeObject = config;
         }
     }
 }
