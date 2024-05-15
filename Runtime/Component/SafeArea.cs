a5715e241d2535f24679364a992ab85d5775f4fe
Li yiqi <liyq@zhongbosoft.com>
2024-05-15 09:45:45 +0800

    v1.3.0

---
+++ Runtime/Component/SafeArea.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace RicKit.UI.Component
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class SafeArea : MonoBehaviour
+    {
+        private RectTransform rectTrans;
+        private void Awake()
+        {
+            Init();
+            AdaptAnchorsValue();
+        }
+        private void Init()
+        {
+            rectTrans = GetComponent<RectTransform>();
+
+            rectTrans.anchorMin = Vector2.zero;
+            rectTrans.anchorMax = Vector2.one;
+            rectTrans.anchoredPosition = Vector2.zero;
+            rectTrans.sizeDelta = Vector2.zero;
+        }
+
+        private void AdaptAnchorsValue()
+        {
+            var maxWidth = Display.main.systemWidth;
+            var maxHeight = Display.main.systemHeight;
+            var safeArea = UnityEngine.Screen.safeArea;
+            var anchorMin = safeArea.position;
+            var anchorMax = safeArea.position + safeArea.size;
+            anchorMin.x /= maxWidth;
+            anchorMin.y /= maxHeight;
+            anchorMax.x /= maxWidth;
+            anchorMax.y /= maxHeight;
+
+            rectTrans.anchorMin = anchorMin;
+            rectTrans.anchorMax = anchorMax;
+        }
+    }
+}
