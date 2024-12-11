a373b50d94e6f5dd58be41851b3d34ffd15d875e
rickytheoldtree <rickyliyiqi@outlook.com>
2024-12-11 17:47:36 +0800

    v2.5.3

---
+++ Runtime/Component/SafeArea.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace RicKit.UI.Component
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class SafeArea : MonoBehaviour
+    {
+        private RectTransform rectTrans;
+        private Rect safeArea;
+        private void Awake()
+        {
+            rectTrans = GetComponent<RectTransform>();
+            rectTrans.anchorMin = Vector2.zero;
+            rectTrans.anchorMax = Vector2.one;
+            rectTrans.anchoredPosition = Vector2.zero;
+            rectTrans.sizeDelta = Vector2.zero;
+            AdaptAnchorsValue();
+        }
+
+        private void LateUpdate()
+        {
+            if(safeArea == Screen.safeArea) return;
+            AdaptAnchorsValue();
+        }
+
+        private void AdaptAnchorsValue()
+        {
+            safeArea = Screen.safeArea;
+            var maxWidth = Display.main.systemWidth;
+            var maxHeight = Display.main.systemHeight;
+            var anchorMin = safeArea.position;
+            var anchorMax = safeArea.position + safeArea.size;
+            anchorMin.x /= maxWidth;
+            anchorMin.y /= maxHeight;
+            anchorMax.x /= maxWidth;
+            anchorMax.y /= maxHeight;
+
+            rectTrans.anchorMin = anchorMin;
+            rectTrans.anchorMax = anchorMax;
+        }
+    }
+}
