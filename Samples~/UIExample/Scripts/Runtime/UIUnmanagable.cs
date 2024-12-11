6deb8168b1e7b89811fa39856c36e67690d0ac97
rickytheoldtree <rickyliyiqi@outlook.com>
2024-12-11 19:52:21 +0800

    v2.5.4

---
+++ Samples~/UIExample/Scripts/Runtime/UIUnmanagable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RicKit.UI.Panels;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RicKit.UIExample.Scripts
+{
+    public class UIUnmanagable : FadeUIPanel
+    {
+        [SerializeField] private Text txtCanInteract;
+        public override void OnESCClick()
+        {
+            
+        }
+
+        private void Update()
+        {
+            txtCanInteract.text = "Can Interact: " + (!UI.IsLockInput() ? "Yes" : "No");
+        }
+    }
+}
