bd9e56abc792951ef082179185324002e1227490
rickytheoldtree <rickyliyiqi@outlook.com>
2024-12-25 17:26:54 +0800

    v2.6.1

---
+++ Editor/EditorReady.cs
@@ -0,0 +1,56 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace RicKit.UI.Editor
+{
+    public static class EditorReady
+    {
+        [InitializeOnLoadMethod]
+        public static void MakeSureSortingLayersReady()
+        {
+            AddSortingLayer("UI", 114514);
+            AddSortingLayer("Blocker", 810975);
+        }
+
+        private static void AddSortingLayer(string sortingLayer, int uniqueID)
+        {
+            if (IsHasSortingLayer(sortingLayer)) return;
+            var tagManager =
+                new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+            var it = tagManager.GetIterator();
+            while (it.NextVisible(true))
+            {
+                if (it.name != "m_SortingLayers") continue;
+                it.InsertArrayElementAtIndex(it.arraySize);
+                var dataPoint = it.GetArrayElementAtIndex(it.arraySize - 1);
+                while (dataPoint.NextVisible(true))
+                {
+                    switch (dataPoint.name)
+                    {
+                        case "name":
+                            dataPoint.stringValue = sortingLayer;
+                            tagManager.ApplyModifiedProperties();
+                            break;
+                        case "uniqueID":
+                            dataPoint.intValue = uniqueID;
+                            tagManager.ApplyModifiedProperties();
+                            break;
+                    }
+                }
+            }
+        }
+
+        private static bool IsHasSortingLayer(string sortingLayer)
+        {
+            var layers = SortingLayer.layers;
+            foreach (var layer in layers)
+            {
+                if (layer.name == sortingLayer)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
