dc04cee1637768a1f3d1f0235aeb1a1ca80895ea
rickytheoldtree <rickyliyiqi@outlook.com>
2025-02-17 09:17:19 +0800

    v3.0.0

---
+++ Runtime/UniversalRenderPipeline/UniversalRenderPipelineCamStackUICam.cs
@@ -0,0 +1,19 @@
+﻿#if USING_URP
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace RicKit.UI.URP
+{
+    [RequireComponent(typeof(Camera))]
+    public class UniversalRenderPipelineCamStackUICam : MonoBehaviour
+    {
+        private void Start()
+        {
+            var camData = GetComponent<Camera>().GetUniversalAdditionalCameraData();
+            var uiCam = UIManager.I.UICamera;
+            if (camData.cameraStack.Contains(uiCam)) return;
+            camData.cameraStack.Add(uiCam);
+        }
+    }
+}
+#endif
