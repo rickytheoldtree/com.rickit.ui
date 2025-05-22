3ac7d1548eb7ac4677318b6d202e051b5239a701
rickytheoldtree <rickyliyiqi@outlook.com>
2025-05-22 14:27:08 +0800

    v3.3.0

---
+++ Runtime/UISettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace RicKit.UI
+{
+    public class UISettings : ScriptableObject
+    {
+        public CameraClearFlags cameraClearFlags = CameraClearFlags.Depth;
+        public Color backgroundColor = new Color(0.1921569f, 0.1921569f, 0.1921569f, 1);
+        public int cullingMask = 32;
+        public int depth;
+        public float nearClipPlane = 1;
+        public float farClipPlane = 15;
+        public Vector2 referenceResolution = new Vector2(1080, 1920);
+        public CanvasScaler.ScreenMatchMode screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+        [Range(0, 1)]
+        public float matchWidthOrHeight = 1f;
+        public string assetPathPrefix = "UI/";
+    }
+}
