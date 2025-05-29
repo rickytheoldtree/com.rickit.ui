2264e3a7cac0e63681f9b97cb4b8db5c8c4d1f3d
rickytheoldtree <rickyliyiqi@outlook.com>
2025-05-29 09:10:21 +0800

    v3.3.2

---
+++ Runtime/Interfaces/IPanelLoader.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace RicKit.UI.Interfaces
+{
+    public interface IPanelLoader
+    {
+        UniTask<GameObject> LoadPrefabAsync(string path);
+        
+        GameObject LoadPrefab(string path);
+    }
+}
