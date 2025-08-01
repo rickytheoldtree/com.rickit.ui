605ca7e7fecd531fe8acba309a9f057657ee7555
rickytheoldtree <rickyliyiqi@outlook.com>
2025-08-01 08:53:49 +0800

    v3.3.6

---
+++ Editor/UISettingsInspector.cs
@@ -0,0 +1,90 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RicKit.UI.Editor
+{
+    [CustomEditor(typeof(UISettings))]
+    public class UISettingsInspector : UnityEditor.Editor
+    {
+        private UISettings settings;
+        private GUIStyle titleStyle;
+
+        private SerializedProperty cameraClearFlags,
+            backgroundColor,
+            cullingMask,
+            depth,
+            nearClipPlane,
+            farClipPlane,
+            referenceResolution,
+            screenMatchMode,
+            matchWidthOrHeight,
+            assetPathPrefix;
+
+        private void OnEnable()
+        {
+            settings = (UISettings)target;
+            cameraClearFlags = serializedObject.FindProperty("cameraClearFlags");
+            backgroundColor = serializedObject.FindProperty("backgroundColor");
+            cullingMask = serializedObject.FindProperty("cullingMask");
+            depth = serializedObject.FindProperty("depth");
+            nearClipPlane = serializedObject.FindProperty("nearClipPlane");
+            farClipPlane = serializedObject.FindProperty("farClipPlane");
+            referenceResolution = serializedObject.FindProperty("referenceResolution");
+            screenMatchMode = serializedObject.FindProperty("screenMatchMode");
+            matchWidthOrHeight = serializedObject.FindProperty("matchWidthOrHeight");
+            assetPathPrefix = serializedObject.FindProperty("assetPathPrefix");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            GUILayout.Label("Camera", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(cameraClearFlags);
+            if (settings.cameraClearFlags == CameraClearFlags.SolidColor || settings.cameraClearFlags == CameraClearFlags.Skybox)
+            {
+                EditorGUILayout.PropertyField(backgroundColor);
+            }
+            EditorGUILayout.PropertyField(cullingMask);
+            EditorGUILayout.PropertyField(depth);
+            EditorGUILayout.PropertyField(nearClipPlane);
+            EditorGUILayout.PropertyField(farClipPlane);
+            EditorGUI.indentLevel--;
+            GUILayout.Label("Canvas", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(referenceResolution);
+            EditorGUILayout.PropertyField(screenMatchMode);
+            if (settings.screenMatchMode == CanvasScaler.ScreenMatchMode.MatchWidthOrHeight)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(matchWidthOrHeight);
+                EditorGUI.indentLevel--;
+            }
+            EditorGUI.indentLevel--;
+            GUILayout.Label("Asset", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(assetPathPrefix);
+            serializedObject.ApplyModifiedProperties();
+        }
+        
+        [MenuItem("RicKit/UI/Open Settings", false, 1)]
+        public static void Open()
+        {
+            if (!AssetDatabase.IsValidFolder("Assets/Resources"))
+            {
+                AssetDatabase.CreateFolder("Assets", "Resources");
+            }
+            var asset = AssetDatabase.LoadAssetAtPath<UISettings>("Assets/Resources/UISettings.asset");
+            if (asset)
+            {
+                Selection.activeObject = asset;
+                return;
+            }
+            asset = CreateInstance<UISettings>();
+            AssetDatabase.CreateAsset(asset, "Assets/Resources/UISettings.asset");
+            AssetDatabase.SaveAssets();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = asset;
+        }
+    }
+}
---
+++ Runtime/Panels/AbstractUIPanel.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RicKit.UI.Panels
+{
+    [RequireComponent(typeof(Canvas), typeof(CanvasGroup), typeof(GraphicRaycaster))]
+    public abstract class AbstractUIPanel : MonoBehaviour
+    {
+        public int OrderInLayer { get; private set; }
+        public bool IsShow =>  gameObject.activeSelf;
+
+        public bool CanInteract => IsShow && CanvasGroup.interactable;
+        protected CanvasGroup CanvasGroup { get; private set; }
+        protected RectTransform CanvasRect { get; private set; }
+        private Canvas Canvas { get; set; }
+        public virtual bool DontDestroyOnClear => false;
+        protected static IUIManager UI => UIManager.I;
+        protected virtual void Awake()
+        {
+            Canvas = GetComponent<Canvas>();
+            Canvas.overrideSorting = true;
+            Canvas.sortingLayerName = "UI";
+            CanvasGroup = GetComponent<CanvasGroup>();
+            CanvasRect = Canvas.GetComponent<RectTransform>();
+        }
+        public async UniTask OnShowAsync()
+        {
+            UI.SetLockInput(true);
+            gameObject.SetActive(true);
+            UI.OnShow?.Invoke(this);
+            CanvasGroup.blocksRaycasts = true;
+            CanvasGroup.interactable = false;
+            await OnAnimationIn(this.GetCancellationTokenOnDestroy());
+            CanvasGroup.blocksRaycasts = true;
+            CanvasGroup.interactable = true;
+            UI.OnShowEnd?.Invoke(this);
+            UI.SetLockInput(false);
+        }
+        public async UniTask OnHideAsync()
+        {
+            UI.SetLockInput(true);
+            UI.OnHide?.Invoke(this);
+            CanvasGroup.blocksRaycasts = true;
+            CanvasGroup.interactable = false;
+            await OnAnimationOut(this.GetCancellationTokenOnDestroy());
+            gameObject.SetActive(false);
+            UI.OnHideEnd?.Invoke(this);
+            UI.SetLockInput(false);
+        }
+
+        public abstract void OnESCClick();
+        protected abstract UniTask OnAnimationIn(CancellationToken cancellationToken);
+
+        protected abstract UniTask OnAnimationOut(CancellationToken cancellationToken);
+
+        public void SetOrderInLayer(int order)
+        {
+            OrderInLayer = order;
+            Canvas.overrideSorting = true;
+            Canvas.sortingOrder = order;
+        }
+
+        public void SetSortingLayer(string layer)
+        {
+            Canvas.overrideSorting = true;
+            Canvas.sortingLayerName = layer;
+        }
+    }
+}
