2df2c89964d8d4db2c2eaf78c122b3438b1a1719
Li yiqi <rickyliyiqi@outlook.com>
2026-01-14 14:26:15 +0800

    v3.6.6

---
+++ Editor/Component/SafeAreaEditor.cs
@@ -0,0 +1,33 @@
+﻿using RicKit.UI.Component;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RicKit.UI.Editor.Component
+{
+    public class SafeAreaEditor : UnityEditor.Editor
+    {
+        [MenuItem("GameObject/UI/SafeArea(Mobile)", false, 0)]
+        public static void CreateSafeArea()
+        {
+            var parent = Selection.activeTransform;
+            if (!parent || !parent.GetComponent<RectTransform>())
+            {
+                var canvas = new GameObject("Canvas").AddComponent<Canvas>();
+                canvas.transform.SetParent(parent);
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                canvas.gameObject.AddComponent<CanvasScaler>();
+                canvas.gameObject.AddComponent<GraphicRaycaster>();
+                parent = canvas.transform;
+            }
+            var safeArea = new GameObject("SafeArea", typeof(RectTransform), typeof(SafeArea));
+            safeArea.transform.SetParent(parent, false);
+            var rect = safeArea.GetComponent<RectTransform>();
+            rect.anchorMin = Vector2.zero;
+            rect.anchorMax = Vector2.one;
+            rect.offsetMin = Vector2.zero;
+            rect.offsetMax = Vector2.zero;
+            Selection.activeGameObject = safeArea;
+        }
+    }
+}
---
+++ Runtime/UniversalRenderPipeline/UniversalRenderPipelineCamStackUICam.cs
@@ -0,0 +1,29 @@
+﻿#if USING_URP
+using UnityEngine;
+
+namespace RicKit.UI.URP
+{
+    [RequireComponent(typeof(Camera))]
+    public class UniversalRenderPipelineCamStackUICam : MonoBehaviour
+    {
+        private Camera cam;
+
+        private void Awake()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        private void OnEnable()
+        {
+            if (UIManager.I == null) return;
+            UIManager.I.RegisterBaseCam(cam);
+        }
+
+        private void OnDisable()
+        {
+            if (UIManager.I == null) return;
+            UIManager.I.UnregisterBaseCam(cam);
+        }
+    }
+}
+#endif
